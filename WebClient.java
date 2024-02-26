ac3dcb7595519f3ec47ea25ca00ad400081a4a11
Prempreet Brar <prempreet.brar@ucalgary.ca>
2024-02-26 10:16:18 -0700

    adding comments and cleaning up code

--- WebClient.java
+++ WebClient.java
@@ -26,21 +26,27 @@ import java.util.regex.Pattern;
 public class WebClient {
 	private static final Logger logger = Logger.getLogger("WebClient"); // global logger
 
-    // Used for parsing URL.                  protocol     hostname   port   pathname
     // The following regex was constructed with help from the website: https://regex101.com/
+    // Used for parsing URL.                  protocol     hostname   port   pathname
     private static final String URL_REGEX = "([a-zA-Z]+)://([^:/]+):?(\\d*)?/(\\S+)";
-    
-    private static final int SUCCESSFUL_TERMINATION = 0;
-    private static final int UNSUCCESSFUL_TERMINATION = -1;
-    private static final int NO_PORT = 0;
     private static final String STRING_TO_BYTE_CHARSET = "US-ASCII";
+
+    // numbers chosen are arbitrary
+    private static final int NO_PORT = 0;
     private static final int NO_BYTE = -1;
 
-    // currently, we lowercase the parsed protocol from the URL; however, we may change
-    // that implementation in the future, in which case we do not want to have to change
-    // every single string comparison in our program.
+    // numbers follow standard Java convention
+    private static final int SUCCESSFUL_TERMINATION = 0;
+    private static final int UNSUCCESSFUL_TERMINATION = -1;
+
+    /* currently, we lowercase the parsed protocol from the URL; however, we may change
+       that implementation in the future, in which case we do not want to have to change
+       every single string comparison in our program.
+    */
     private static final String HTTP = "http";
     private static final String HTTPS = "https";
+
+    // as specified in assignment
     private static final int HTTP_DEFAULT_PORT = 80;
     private static final int HTTPS_DEFAULT_PORT = 443;
     private static final String SUCCESS_STATUS_PHRASE = "OK";
@@ -59,6 +65,8 @@ public class WebClient {
     private OutputStream outputStream;
     private FileOutputStream fileOutputStream;
     private static final int EOF = -1;
+    private static final int OFFSET = 0;
+    // arbitrary
     private static final int BUFFER_SIZE = 4096;
 
     /**
@@ -124,6 +132,7 @@ public class WebClient {
             hostname = matcher.group(2).toLowerCase();
             String portAsString = matcher.group(3);
 
+            // assign a default port if none is provided
             if (portAsString.isEmpty()) {
                 switch (protocol) {
                     case HTTP:
@@ -137,6 +146,7 @@ public class WebClient {
                 port = Integer.valueOf(portAsString);
             }
             pathname = matcher.group(4);
+            // the file is always the last parth of the pathname
             String[] subdirectories = pathname.split("/");
             filename = subdirectories[subdirectories.length - 1];
         }
@@ -153,7 +163,7 @@ public class WebClient {
             case HTTP:
                 socket = new Socket(hostname, port);
                 break;
-            // if the protocol is HTTP, we need a Secure Socket Layer (SSL) Socket.
+            // if the protocol is HTTPS, we need a Secure Socket Layer (SSL) Socket.
             case HTTPS:
                 SSLSocketFactory factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
                 socket = (SSLSocket) factory.createSocket(hostname, port);
@@ -193,67 +203,129 @@ public class WebClient {
      * @throws IOException 
      */
     private void sendGetRequest(String getRequest) throws IOException {
-        System.out.println(getRequest);
-
         byte[] getRequestBytes = getRequest.getBytes(STRING_TO_BYTE_CHARSET);
         outputStream.write(getRequestBytes);
             
         /*
-        * flush to ensure request is actually written to the socket; we can also shutdown
-        * the output stream as no further requests need to be sent to the server
+        * flush to ensure request is actually written to the socket.
         */
         outputStream.flush();
+        System.out.println(getRequest);
     } 
-    
+
+    /**
+     * Check if the response was successful and if so, read the payload.
+     * 
+     * @throws IOException 
+     */
     private void readResponse() throws IOException {
-        if (successfullyReadHeaderLines()) {
+        if (wasRequestSuccessful()) {
             readPayload();
         }
     }
 
-    private boolean successfullyReadHeaderLines() throws IOException {
+    /**
+     * Reads the response byte by byte, checking for the status code and
+     * status phrase to see if the request was successful, and parsing all the
+     * headers from the response. 
+     * 
+     * @throws IOException 
+     * @return A boolean denoting whether or not the request was successful. 
+     */
+    private boolean wasRequestSuccessful() throws IOException {
+        /*
+            initially, we have not read any bytes from the response. We need
+            both prevByte and currByte because we need to keep track of two
+            bytes sequentially (to see if we've encountered \r\n).
+        */
         int prevByte = NO_BYTE;
         int currByte = NO_BYTE;
-
+        
+        /*  the response status line is formatted as: protocol statusCode statusPhrase
+            when we split on the status line, we use an index of 0 and 1 to access the code and phrase
+            respectively
+         */
         int STATUS_CODE = 1;
         int STATUS_PHRASE = 2;
 
+        /*
+         * We haven't yet read the response, so the current line and the response are empty. 
+         * In addition, we need a separate boolean for knowing if we are on the firstLine, because this is how
+         * we know to check for the status code and phrase. 
+         */
         String currLine = "";
         boolean readFirstLine = false;
         String response = "";
 
-        boolean flag = true;
+        // assume the request was successful unless we find otherwise
+        boolean requestWasSuccessful = true;
 
         while ((currByte = inputStream.read()) != EOF) {
             currLine += (char) currByte;
 
+            /* 
+             * Java implicitly converts the char to its ASCII value when
+             * comparing with prevByte and currByte. This is why we are able 
+             * to make this comparison.
+             */
             if (prevByte == '\r' && currByte == '\n') {
                 response += currLine;
 
+                /*
+                 * Check that both the status code and status phrase are what we expect. 
+                 * We need to trim the status_phrase to get rid of extra white space before
+                 * or after. We don't need to trim on the status_code since Integer.valueOf
+                 * will automatically deal with that white space anyway. 
+                 */
                 if (!readFirstLine) {
                     String[] statusLine = currLine.split(" ");
 
                     if (Integer.valueOf(statusLine[STATUS_CODE]) != SUCCESS_STATUS_CODE ||
                         !(statusLine[STATUS_PHRASE].trim().equals(SUCCESS_STATUS_PHRASE))
                     ) {
-                        flag = false;
+                        /* 
+                            if the request was unsuccessful, we set the boolean to false but
+                            do not immediately break out of the loop. This is because we still 
+                            need to finish reading all headers.
+                        */
+                        requestWasSuccessful = false;
                     }
                     readFirstLine = true;
                 }
                 
+                /*
+                 * If we've encountered a line consisting solely of \r\n, this means
+                 * we've found the end of our header lines. We can exit the loop
+                 * as we no longer want to parse the remainder of the response (doing
+                 * so would mean we are reading the payload).
+                 */
                 if (currLine.equals("\r\n")) {
                     break;
                 }
+
+                /* 
+                 * If you've found the end of a regular header line, then "reset"
+                 * it to begin reading the next header line. This is to ensure that
+                 * we do not add duplicate or redundant info to our response when
+                 * printing to console. 
+                 */
                 currLine = "";
                 prevByte = NO_BYTE;
                 currByte = NO_BYTE;
             }
+
             prevByte = currByte;
         }
+
         System.out.println(response);
-        return flag;
+        return requestWasSuccessful;
     }
 
+    /**
+     * Reads the payload of the response and creates a local file.
+     * 
+     * @throws IOException 
+     */
     private void readPayload() throws IOException {
         /*
         * The numBytes tells us how many bytes we actually read from the server; this may
@@ -266,7 +338,7 @@ public class WebClient {
         fileOutputStream = new FileOutputStream(filename);
 
         while ((numBytes = inputStream.read(buffer)) != EOF) {
-            fileOutputStream.write(buffer, 0, numBytes);
+            fileOutputStream.write(buffer, OFFSET, numBytes);
         }
 
         /*
@@ -298,6 +370,11 @@ public class WebClient {
             wasSuccessful = false;
             e.printStackTrace();
         }
+        /*
+         * We must always close our resources, regardless of whether or not 
+         * the request was successful. We close them in reverse chronological 
+         * order.
+         */
         finally {
             closeGracefully(
                 fileOutputStream,
